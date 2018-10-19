4e3e5dc005b0e7748f4f09f81e7e21637e0a4994
ytknzw <ytknzw@gmail.com>
2018-10-19 21:21:03 +0900

    PyQt update
    Update from PyQt4 to PyQt5.

--- espresso.py
+++ espresso.py
@@ -12,18 +12,18 @@ from threading import Timer
 try:
 	from PySide import QtCore, QtGui, QtSvg
 except ImportError:
-	from PyQt4 import QtCore, QtGui, QtSvg
+	from PyQt5 import QtCore, QtGui, QtSvg, QtWidgets
 
 
-class TrayIcon(QtGui.QSystemTrayIcon):
+class TrayIcon(QtWidgets.QSystemTrayIcon):
 	def __init__(self, inhibitor, parent):
 		super().__init__(parent)
-		
+
 		self.ICON_EMPTY_CUP     = QtGui.QIcon(os.path.join(os.path.dirname(__file__), "Empty_Cup.svg"))
 		self.ICON_FULL_CUP      = QtGui.QIcon(os.path.join(os.path.dirname(__file__), "Full_Cup.svg"))
 		self.ICON_CRYSTAL_CLOCK = QtGui.QIcon(os.path.join(os.path.dirname(__file__), "Crystal_Clock.svg"))
-		self.ICON_CLOSE         = self.parent().style().standardIcon(QtGui.QStyle.SP_DialogCloseButton)
-		
+		self.ICON_CLOSE         = self.parent().style().standardIcon(QtWidgets.QStyle.SP_DialogCloseButton)
+
 		self.inhibitor = inhibitor
 		self.timer = Timer(0, self.Event) # default timer never runs
 		self.UnInhibit()
@@ -32,8 +32,8 @@ class TrayIcon(QtGui.QSystemTrayIcon):
 		self.activated.connect(self.Event)
 
 	def BuildMenu(self):
-		menu = QtGui.QMenu()
-		
+		menu = QtWidgets.QMenu()
+
 		inhibit_menu = menu.addMenu(self.ICON_CRYSTAL_CLOCK, "Inhibit for")
 		for minutes in [1, 5, 10, 15, 30]:
 			suffix = "s" if minutes > 1 else ""
@@ -42,17 +42,17 @@ class TrayIcon(QtGui.QSystemTrayIcon):
 			suffix = "s" if hours > 1 else ""
 			inhibit_menu.addAction(str(hours)+" hour"+suffix).triggered.connect(lambda m = hours*60 : self.Inhibit(m))
 		inhibit_menu.addAction("Indefinitely").triggered.connect(self.Inhibit)
-		
+
 		menu.addAction(self.ICON_CLOSE, "Quit").triggered.connect(self.Quit)
 		return menu
 
 	def Event(self, reason):
-		if reason == QtGui.QSystemTrayIcon.Trigger:
+		if reason == QtWidgets.QSystemTrayIcon.Trigger:
 			if not self.inhibitor.Inhibited:
 				self.Inhibit()
 			else:
 				self.UnInhibit()
-		elif reason == QtGui.QSystemTrayIcon.MiddleClick:
+		elif reason == QtWidgets.QSystemTrayIcon.MiddleClick:
 			self.Quit()
 
 	def Inhibit(self, timeout_minutes = None):
@@ -62,12 +62,12 @@ class TrayIcon(QtGui.QSystemTrayIcon):
 			self.timer.start()
 		self.setIcon(self.ICON_FULL_CUP)
 		self.inhibitor.Inhibit()
-		
+
 	def UnInhibit(self):
 		self.timer.cancel()
 		self.setIcon(self.ICON_EMPTY_CUP)
 		self.inhibitor.UnInhibit()
-		
+
 	def Quit(self):
 		self.UnInhibit()
 		self.deleteLater() # prevents PyQt4 segfault
@@ -76,9 +76,9 @@ class TrayIcon(QtGui.QSystemTrayIcon):
 
 if __name__ == "__main__":
 	import inhibitors
-	
-	app = QtGui.QApplication(sys.argv)
-	if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
+
+	app = QtWidgets.QApplication(sys.argv)
+	if not QtWidgets.QSystemTrayIcon.isSystemTrayAvailable():
 		QtGui.QMessageBox.critical(None, QtCore.QObject.tr(app, "Espresso"), QtCore.QObject.tr(app, "No system tray available"))
 		sys.exit(1)
 	icon = TrayIcon(inhibitors.AutoSelect(), app);
